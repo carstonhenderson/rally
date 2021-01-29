93ea122aebaf0833ecef80449a22cabab25d1205
Carston Henderson <carstonhenderson@icloud.com>
2021-01-29 16:22:26 -0700

    update notification component styles

--- components/notification.js
+++ components/notification.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { roles } from '../hay.config'
 
 const Notification = ({ text, role }) => (
-  <div
-    className={`-mt-4 -mx-4 bg-${roles.background[role]} p-4 text-${roles.text[role]} text-center`}
-  >
-    {text}
-  </div>
+	<div className="absolute w-full inset-x-0">
+		<div
+			className={`bg-${roles.background[role]} px-4 py-6 text-${roles.text[role]} text-center shadow-2xl`}
+		>
+			{text}
+		</div>
+	</div>
 )
 
 export default Notification
