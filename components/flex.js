218578c13d45b798dccdde23eb996f00d192a5d1
Carston Henderson <carstonhenderson@icloud.com>
2021-01-29 16:20:56 -0700

    add width prop to flex component

--- components/flex.js
+++ components/flex.js
@@ -2,47 +2,49 @@ import React, { Children } from 'react'
 import { spacing } from '../hay.config'
 
 const Flex = ({
-  children,
-  direction,
-  justify,
-  align,
-  alignContent,
-  space,
-  height,
-  constrain,
-  wrap
+	children,
+	direction,
+	justify,
+	align,
+	alignContent,
+	space,
+	height,
+	width,
+	constrain,
+	wrap
 }) => {
-  const directionClass = direction ? `flex-${direction}` : ''
-  const justifyClass = justify ? `justify-${justify}` : ''
-  const alignClass = align ? `items-${align}` : ''
-  const heightClass = height ? `h-${height}` : ''
-  const constrainClass =
-    direction === 'col' ? 'flex-1 min-h-0' : 'flex-1 min-w-0'
-  const wrapClass = wrap ? 'flex-wrap' : ''
-  const alignContentClass = alignContent ? `content-${alignContent}` : ''
+	const directionClass = direction ? `flex-${direction}` : ''
+	const justifyClass = justify ? `justify-${justify}` : ''
+	const alignClass = align ? `items-${align}` : ''
+	const heightClass = height ? `h-${height}` : ''
+	const widthClass = width ? `w-${width}` : ''
+	const constrainClass =
+		direction === 'col' ? 'flex-1 min-h-0' : 'flex-1 min-w-0'
+	const wrapClass = wrap ? 'flex-wrap' : ''
+	const alignContentClass = alignContent ? `content-${alignContent}` : ''
 
-  let spaceClass = direction === 'col' ? 'mb' : 'mr'
+	let spaceClass = direction === 'col' ? 'mb' : 'mr'
 
-  spaceClass = space ? `${spaceClass}-${spacing[space]}` : ''
+	spaceClass = space ? `${spaceClass}-${spacing[space]}` : ''
 
-  return (
-    <div
-      className={`flex ${directionClass} ${justifyClass} ${alignClass} ${heightClass} ${wrapClass} ${alignContentClass}`}
-    >
-      {Children.map(children, (child, index) => (
-        <div
-          className={`${index === children.length - 1 ? '' : spaceClass} ${
-            constrain == index ? constrainClass : ''
-          }
+	return (
+		<div
+			className={`flex ${directionClass} ${justifyClass} ${alignClass} ${heightClass} ${widthClass} ${wrapClass} ${alignContentClass}`}
+		>
+			{Children.map(children, (child, index) => (
+				<div
+					className={`${index === children.length - 1 ? '' : spaceClass} ${
+						constrain == index ? constrainClass : ''
+					}
             ${constrain === 'all' ? constrainClass : ''}
           `}
-          key={index}
-        >
-          {child}
-        </div>
-      ))}
-    </div>
-  )
+					key={index}
+				>
+					{child}
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default Flex
