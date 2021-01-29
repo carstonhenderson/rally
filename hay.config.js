13852afc64d64c4fad9e0fab8d79fb719f4807ab
Carston Henderson <carstonhenderson@icloud.com>
2021-01-29 16:20:15 -0700

    update mood colors

--- hay.config.js
+++ hay.config.js
@@ -4,106 +4,106 @@ import tailwindConfig from './tailwind.config.js'
 const fullConfig = resolveConfig(tailwindConfig)
 
 export const spacing = {
-  xs: 2,
-  sm: 4,
-  md: 8,
-  lg: 16,
-  xl: 32
+	xs: 2,
+	sm: 4,
+	md: 8,
+	lg: 16,
+	xl: 32
 }
 
 export const moods = {
-  great: {
-    text: 'Great',
-    color: 'purple-500',
-    base: 'green',
-    raw: fullConfig.theme.colors.green[500]
-  },
-  good: {
-    text: 'Good',
-    color: 'green-500',
-    base: 'teal',
-    raw: fullConfig.theme.colors.teal[500]
-  },
-  fine: {
-    text: 'Fine',
-    color: 'yellow-500',
-    base: 'blue',
-    raw: fullConfig.theme.colors.blue[500]
-  },
-  bad: {
-    text: 'Bad',
-    color: 'orange-500',
-    base: 'indigo',
-    raw: fullConfig.theme.colors.indigo[500]
-  },
-  terrible: {
-    text: 'Terrible',
-    color: 'red-500',
-    base: 'gray',
-    raw: fullConfig.theme.colors.gray[500]
-  }
+	great: {
+		text: 'Great',
+		color: 'green-500',
+		base: 'green',
+		raw: fullConfig.theme.colors.green[500]
+	},
+	good: {
+		text: 'Good',
+		color: 'teal-500',
+		base: 'teal',
+		raw: fullConfig.theme.colors.teal[500]
+	},
+	fine: {
+		text: 'Fine',
+		color: 'blue-500',
+		base: 'blue',
+		raw: fullConfig.theme.colors.blue[500]
+	},
+	bad: {
+		text: 'Bad',
+		color: 'indigo-500',
+		base: 'indigo',
+		raw: fullConfig.theme.colors.indigo[500]
+	},
+	terrible: {
+		text: 'Terrible',
+		color: 'gray-500',
+		base: 'gray',
+		raw: fullConfig.theme.colors.gray[500]
+	}
 }
 
 export const roles = {
-  background: {
-    primary: 'green-500',
-    secondary: 'green-500',
-    info: 'purple-500',
-    success: 'green-500',
-    danger: 'red-500',
-    hover: {
-      primary: 'green-600',
-      secondary: 'green-200',
-      info: 'purple-600',
-      success: 'green-600',
-      danger: 'red-600'
-    }
-  },
-  text: {
-    primary: 'white',
-    secondary: 'green-500',
-    info: 'white',
-    success: 'white',
-    danger: 'white'
-  }
+	background: {
+		primary: 'green-500',
+		secondary: 'green-500',
+		info: 'purple-500',
+		success: 'green-500',
+		danger: 'red-500',
+		hover: {
+			primary: 'green-600',
+			secondary: 'green-200',
+			info: 'purple-600',
+			success: 'green-600',
+			danger: 'red-600'
+		}
+	},
+	text: {
+		primary: 'white',
+		secondary: 'green-500',
+		info: 'white',
+		success: 'white',
+		danger: 'white'
+	}
 }
 
 export const emotions = {
-  positive: [
-    'calm',
-    'confident',
-    'content',
-    'eager',
-    'ecstatic',
-    'engaged',
-    'excited',
-    'grateful',
-    'happy',
-    'humorous',
-    'inspired',
-    'loving',
-    'motivated',
-    'optimistic',
-    'passionate'
-  ],
-  negative: [
-    'angry',
-    'afraid',
-    'annoyed',
-    'anxious',
-    'ashamed',
-    'bored',
-    'confused',
-    'demoralized',
-    'depressed',
-    'disappointed',
-    'embarrassed',
-    'frustrated',
-    'guilty',
-    'insecure',
-    'lonely',
-    'sad',
-    'stressed',
-    'worried'
-  ]
+	positive: [
+		'calm',
+		'confident',
+		'content',
+		'eager',
+		'ecstatic',
+		'engaged',
+		'excited',
+		'grateful',
+		'happy',
+		'humorous',
+		'inspired',
+		'loving',
+		'motivated',
+		'optimistic',
+		'passionate'
+	],
+	negative: [
+		'angry',
+		'afraid',
+		'annoyed',
+		'anxious',
+		'ashamed',
+		'bored',
+		'confused',
+		'demoralized',
+		'depressed',
+		'disappointed',
+		'embarrassed',
+		'frustrated',
+		'guilty',
+		'insecure',
+		'lonely',
+		'sad',
+		'stressed',
+		'worried'
+	]
 }
