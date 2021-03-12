6789e8f59cde66fe66bcf3daf1babf1a15a69c1d
Carston Henderson <carstonhenderson@icloud.com>
2021-03-12 14:29:49 -0700

    specify theme colors for tailwind upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,28 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans]
-      },
-      flexGrow: {
-        '2': 2
-      }
-    }
-  }
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.black,
+			white: colors.white,
+			gray: colors.gray,
+			green: colors.green,
+			indigo: colors.indigo,
+			red: colors.rose,
+			blue: colors.blue,
+			teal: colors.teal,
+			yellow: colors.amber
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Inter', ...defaultTheme.fontFamily.sans]
+			},
+			flexGrow: {
+				'2': 2
+			}
+		}
+	}
 }
